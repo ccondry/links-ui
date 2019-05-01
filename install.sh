03fe6a17fb4e87ab63f5f6064866a490cc2f1754
Coty Condry <ccondry@cisco.com>
2019-05-01 15:28:12 -0500

    make sure target directory exists during install script

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ if [ $? -eq 0 ]; then
     echo "npm build successful"
     #echo "removing old web files"
     #rm -rf /var/www/html/
+    echo "making sure the directory exists"
+    mkdir -p /var/www/html/links/
     echo "copying new web files"
     cp -rf dist/* /var/www/html/links/
   else
